5885c011af84ca9e316efe877a876eb09943c1e1
Huy Dang <huy.dang@hitachivantara,com>
2020-07-15 17:21:44 +0700

    save

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
-
+from .views import HomeView, ArticleDetailsView, AddPostView
 urlpatterns = [
-    path('', views.home, name="home")
+    # path('', views.home, name="home")
+    path('', HomeView.as_view(), name='home'),
+    #primary key
+    path('article/<int:pk>',ArticleDetailsView.as_view(), name="article-details"),
+    path('add_post/',AddPostView.as_view(), name="add_post"),
+
 ]
