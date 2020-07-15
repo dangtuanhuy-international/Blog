872dbc6388a765668714efbdbed07369e18f7f2f
Huy Dang <huy.dang@hitachivantara,com>
2020-07-15 17:55:47 +0700

    save

--- theblog/views.py
+++ theblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from theblog.models import Post
+from .forms import PostForm
 # Create your views here.
 # def home(request):
 #     return render(request, 'home.html', {})
@@ -12,5 +13,6 @@ class ArticleDetailsView(DetailView):
     template_name = 'acticle_detail.html'
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__'
+    #fields = '__all__'
